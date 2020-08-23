d45a6dee4e02cb3c2a79d981f591b7426c5e3fde
thanhtuancris <thanhtuancristiano@gmail.com>
2020-08-24 00:48:21 +0700

    update 2

--- controllers/c_home.js
+++ controllers/c_home.js
@@ -1,6 +1,6 @@
 const path = require('path')
 let getHome = (req, res) => {
-  return res.sendFile(path.join(`${__dirname}/../views/index.html`))
+  return res.sendFile(path.join(`${__dirname}/../views/home.html`))
 }
 module.exports = {
   getHome: getHome
--- server.js
+++ server.js
@@ -3,19 +3,12 @@ const app = express();
 const initRoutes = require('./routes/r_mailer');
 
 app.use(express.urlencoded({extended: true}));
-app.set('view engine', 'ejs');
-app.set('views', './views');
+
 //Khoi tao cac Routes cho ung dung
 initRoutes(app);
 
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) =>{
-    res.render('home');
-});
-
 app.listen(port, () => {
     console.log("Server lisening on port" + port);
 });
-
-module.exports = app;
